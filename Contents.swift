7123a62830830a8fae0218b6dd6b1f3022741eeb
LeeeeSuHyeon <tngus0673@naver.com>
2023-12-03 15:54:48 +0900

    markup 사용

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+/*:
+# Welcome to Playgorunds
+This is your *first* playground
+* The use of **Quyck Look**
+* Placing results **in-line** with the code
+*/
+
+
+
