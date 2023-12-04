12e23beadf9dc9deb1459262045c7ccfaecc3000
LeeeeSuHyeon <tngus0673@naver.com>
2023-12-04 15:49:00 +0900

    스위프트 연산자와 표현식

---
+++ Pages/Expression.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//let y = 3
+//let z = ~y
+//print(z)    // -4
+
+//let x = 171
+//let y = 3
+//let z = x & y
+//print(z)    // 3
+
+//let x = 171
+//let y = 3
+//let z = x | y
+//print(z)    // 171
+
+
+//let x = 171
+//let y = 3
+//let z = x ^ y
+//print(z)    // 342
+
+//let x = 171
+//let z = x << 2
+//print(z)    // 684
+
+
+//let x = 171
+//let z = x >> 1
+//print(z)    // 85
+
+let x = 171
+let z = x >> 2
+print(z)    // 42
